3163d6a592a3d16fb4cbe7fc7e685d706f6e83c5
tanq <tanq@amazon.com>
2026-02-22 11:48:10 +0530

    Add custom S3 bucket resource

---
+++ amplify/backend/custom/customResourcefddb1f82/cdk-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class cdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    new cdk.CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+
+    // Add custom S3 bucket
+    new s3.Bucket(this, 'CustomGen1Bucket', {
+      bucketName: `amplify-gen1-custom-v2-${cdk.Stack.of(this).account}`,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
